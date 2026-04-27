bbb35974886e1bae44e5fcd23d27cbd1bba87a4e
Shoaib Ahmad <168672136+shoaib-ahmadd@users.noreply.github.com>
2026-04-27 12:13:21 +0530

    Update ocr_engine.py

--- core/ocr_engine.py
+++ core/ocr_engine.py
@@ -1,5 +1,11 @@
+from dataclasses import dataclass
+
+@dataclass
+class OCRConfig:
+    lang: str = "en"
+
 class OCREngine:
     def__init__(self, *args, **kwargs):
     pass
-    def extract_text(self, image):
-        return ""
+    def extract_text(self, image)
+    return ""
